b9c6c9d6b5e6746ee3c5a326b4bb6aaff59ffd88
Liao Zhu <liao.zhu.cs@gmail.com>
2022-07-07 19:58:58 -0500

    Progress on Selection Sort

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Containers
 let container = document.getElementById("container");
-let sortedContainer = document.getElementById("sorted-container");
 let unsortedContainer = document.getElementById("unsorted-container");
 
 // UI Stuff
@@ -10,13 +9,12 @@ let input = document.getElementById("number-input");
 let random = document.getElementById("random");
 let clear = document.getElementById("clear");
 let bubble = document.getElementById("bubble");
-let merge = document.getElementById("merge");
+let selection = document.getElementById("selection");
 let heap = document.getElementById("heap");
 let quick = document.getElementById("quick");
 
 // Initilalize 
 let randomArray = [];
-let unsortedBlock;
 
 // Extra 
 let finished = false;
@@ -27,66 +25,136 @@ let finished = false;
 // Create Random Array 
 function createRandom() {
     clearList();
-    for (let i = 0; i < 50; i++) {
-        let randomInt = Math.floor(Math.random() * 501);
-        if (randomInt != 0) {
-            randomArray.push(randomInt);
-        }
+    for (let i = 0; i < 5; i++) {
+
+        // To generate random values from 1 to 100
+        const value = Math.floor(Math.random() * 100) + 1;
+        
+        // To create element "div"
+        const bar = document.createElement("div");
+    
+        // To add class "bar" to "div"
+        bar.classList.add("bar");
+    
+        // Provide height to the bar
+        bar.style.height = `${value * 3}px`;
+    
+        // Translate the bar towards positive X axis
+        bar.style.transform = `translateX(${i * 3}px)`;
+        
+        // To create element "label"
+        const barLabel = document.createElement("label");
+    
+        // To add class "bar_id" to "label"
+        barLabel.classList.add("bar-label");
+        
+        // Assign value to "label"
+	    barLabel.innerHTML = value;
+
+        // Append "Label" to "div"  
+        bar.appendChild(barLabel);
+    
+        // Append "div" to "data-container div"
+        unsortedContainer.appendChild(bar);
     }
-    createUnsortedDivs(randomArray);
-}
-// Clear List
-function clearList() {
-    finished = true;
-    sortedContainer.innerHTML = "";
-    unsortedContainer.innerHTML = "";
-    randomArray = [];
 }
 
 // Bubble Sort
-//
-function bubbleSwap(arr, x, y) {
-    let temp = arr[x];
-    arr[x] = arr[y];
-    arr[y] = temp;
-}
+// function bubbleSwap(arr, x, y) {
+//     let temp = arr[x];
+//     arr[x] = arr[y];
+//     arr[y] = temp;
+// }
 
-function bubbleSort(arr) {
-    if (finished) {
-        for (let i = 0; i < arr.length; i++) {
-            for (let j = 0; j < arr.length-i; j++) {
-                if (arr[j] > arr[j+1]) {
-                    bubbleSwap(arr, j, j+1);
+// function bubbleSort(arr) {
+//     if (finished) {
+//         for (let i = 0; i < arr.length; i++) {
+//             for (let j = 0; j < arr.length-i; j++) {
+//                 if (arr[j] > arr[j+1]) {
+//                     bubbleSwap(arr, j, j+1);
+//                 }
+//             }
+//         }
+//         createDivs(arr);
+//         finished = false;
+//     }
+// }
+
+// Selection Sort
+async function selectionSort() {
+    let bars = document.querySelectorAll(".bar");
+    console.log(bars);
+    // Assign 0 to min
+    var min = 0;
+    for (var i = 0; i < bars.length; i ++) {
+    
+        // Assign i to min
+        min = i;
+    
+        // Provide darkblue color to the ith bar
+        bars[i].style.backgroundColor = "darkblue";
+
+        for (var j = i + 1; j < bars.length; j ++) {
+    
+            // Provide red color to the jth bar
+            bars[j].style.backgroundColor = "red";
+            
+            // To pause the execution of code
+            await new Promise((resolve) =>
+                setTimeout(() => {
+                resolve();
+                }, 500)
+            );
+    
+            // To store values of bars
+            let val1 = bars[j].childNodes[0].innerHTML;
+            let val2 = bars[min].childNodes[0].innerHTML;
+            console.log(val1);
+            console.log(val2);
+            
+            // Compare val1 & val2
+            if (val1 < val2) {
+                if (min !== i) {
+    
+                // Provide skyblue color to the (min-idx)th bar
+                bars[min].style.backgroundColor = " rgb(24, 190, 255)";
                 }
+                min = j;
+            } 
+            else {
+                // Provide skyblue color to the jth bar
+                bars[j].style.backgroundColor = " rgb(24, 190, 255)";
             }
         }
-        createDivs(arr);
-        finished = false;
+    
+        // To swap ith and (min)th bar
+        var temp1 = bars[min].style.height;
+        var temp2 = bars[min].childNodes[0].innerText;
+        bars[min].style.height = bars[i].style.height;
+        bars[i].style.height = temp1;
+        bars[min].childNodes[0].innerText = bars[i].childNodes[0].innerText;
+        bars[i].childNodes[0].innerText = temp2;
+        
+        // To pause the execution of code for 300 milliseconds
+        await new Promise((resolve) =>
+        setTimeout(() => {
+            resolve();
+        }, 5)
+        );
+    
+        // Provide skyblue color to the (min-idx)th bar
+        bars[min].style.backgroundColor = " rgb(24, 190, 255)";
+    
+        // Provide lightgreen color to the ith bar
+        bars[i].style.backgroundColor = " rgb(49, 226, 13)";
     }
 }
-//
-// End Bubble Sort
 
-// Create Divs
-function createUnsortedDivs(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        unsortedBlock = document.createElement("div");
-        unsortedBlock.classList.add("unsorted-block");
-        unsortedBlock.setAttribute("id", "unsorted-block");
-        unsortedBlock.style.height = arr[i] + "px";
-        unsortedContainer.appendChild(unsortedBlock);
-    }
-}
-
-// Test
-function bubbleDisplay(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        let block = document.createElement("div");
-        block.classList.add("block");
-        block.setAttribute("id", "sorted-block");
-        block.innerHTML = arr[i];
-        sortedContainer.appendChild(block);
-    }   
+// Clear List
+function clearList() {
+    finished = true;
+    unsortedContainer.innerHTML = "";
+    randomArray = [];
 }
 
 //
@@ -99,11 +167,11 @@ function bubbleDisplay(arr) {
 
 // Sort Buttons
 bubble.addEventListener("click", () => {
-    bubbleDisplay(randomArray);
+
 }); 
 
-merge.addEventListener("click", () => {
-
+selection.addEventListener("click", () => {
+    selectionSort();
 }); 
 
 heap.addEventListener("click", () => {
